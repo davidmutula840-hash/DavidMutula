ebb12b3367af3e13944d790ff0d8f3042917c5c6
davidmutula840-hash <davidmutula840@gmail.com>
2025-10-14 20:41:18 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Mobile Menu Toggle
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+
